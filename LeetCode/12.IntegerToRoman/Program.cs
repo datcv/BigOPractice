205b2796c50db45f407f6cca170a27d8844c6673
David Vo <datcv@hotmail.com>
2020-02-20 15:41:47 +1100

    Add leetcode problem - Integer to roman

---
+++ LeetCode/12.IntegerToRoman/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace _12.IntegerToRoman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int num = 1994;
+            Console.WriteLine($"{num} = {IntToRoman(num)}");
+        }
+
+        public static string IntToRoman(int num) {
+            var arr = new char[] {'I', 'V', 'X','L','C','D','M'};
+            var stack = new System.Collections.Generic.Stack<char>();
+            var oneIndex = 0;
+            var fiveIndex = 1;
+            var tenIndex = 2;
+
+            while(num > 0)
+            {
+                var mod = num % 10;                
+                switch(mod)
+                {
+                    case 4:
+                        stack.Push(arr[fiveIndex]);
+                        stack.Push(arr[oneIndex]);
+                    break;
+                    case 9:
+                        stack.Push(arr[tenIndex]);
+                        stack.Push(arr[oneIndex]);
+                    break;
+
+                    default:
+                        for (int i = 0; i < mod % 5; i++)
+                        {
+                            stack.Push(arr[oneIndex]);
+                        }
+
+                        if (mod >= 5)
+                        {
+                            stack.Push(arr[fiveIndex]);
+                        }
+                    break;
+                }
+                
+                num /= 10;
+
+                oneIndex += 2;
+                fiveIndex += 2;
+                tenIndex += 2;
+            }
+
+            var charArr = new char[stack.Count];
+            
+            for (int i = 0; i < charArr.Length; i++)
+            {
+                charArr[i] = stack.Pop();
+            }
+            
+            return new string(charArr);
+        }
+    }
+}
