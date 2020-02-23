72e6ceb286a17661d412fdd05f631fe211dd0593
David Vo <datcv@hotmail.com>
2020-02-23 14:31:25 +1100

    Sorted Square Array

---
+++ RandomSource/SortedSquareArray/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SortedSquareArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new int[]{-7, -1, 4, 8, 10};
+            var output = SortedSquareArray(input);
+
+
+            Console.WriteLine("Input: " + string.Join(", ", input));
+            Console.WriteLine("Output: " + string.Join(", ", output));
+        }
+
+        private static int[] SortedSquareArray(int[] arr)
+        {
+            if (arr == null || arr.Length == 0)
+            {
+                return null;
+            }
+
+            var squareArr = new int[arr.Length];
+            int i = 0, j = arr.Length - 1, sIndex = squareArr.Length - 1;
+
+            while (i <= j)
+            {
+                if (arr[i] * arr[i] > arr[j] * arr[j])
+                {
+                    squareArr[sIndex] = arr[i] * arr[i];
+                    i++;
+                }
+                else
+                {
+                    squareArr[sIndex] = arr[j] * arr[j];
+                    j--;
+                }
+
+                sIndex--;
+            }
+
+            return squareArr;
+        }
+    }
+}
---
+++ RandomSource/SortedSquareArray/README.md
@@ -0,0 +1,11 @@
+# Sorted Square Array
+ Given a sorted array of integers. Write a function that returns a sorted array containing the squares of those intergers.  
+
+ Input array length: 1 <= n <= 10000  
+ Element: -10000 <= array[i] <= 10000  
+ Input array will be sorted  
+ Output array must be sorted  
+
+ **Example:**  
+Input: [-7, -1, 4, 8, 10]  
+Output: [1, 16, 49, 64, 100]
