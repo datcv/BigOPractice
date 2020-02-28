c8b27ae4c94340cadfc45fbd19008ecbd0051ee7
David Vo <datcv@hotmail.com>
2020-02-28 17:07:32 +1100

    Product of Array Except Self

---
+++ LeetCode/238.ProductOfArrayExceptSelf/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _238.ProductOfArrayExceptSelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputArr = new int[]{1, 2, 3, 4, 9};
+            var outputArr = ProductExceptSelf(inputArr);
+
+            Console.WriteLine("Input: [" + string.Join(", ", inputArr) + "]");
+            Console.WriteLine("Output: [" + string.Join(", ", outputArr) + "]");
+        }
+
+
+        public static int[] ProductExceptSelf(int[] nums)
+        {
+            var arr = new int[nums.Length];
+            int c = 1;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                arr[i] = c;
+                c *= nums[i];
+            }
+
+            c = 1;
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                arr[i] *= c;
+                c *= nums[i];
+            }
+
+            return arr;
+        }
+    }
+}
