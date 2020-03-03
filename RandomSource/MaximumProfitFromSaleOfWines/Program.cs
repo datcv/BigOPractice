0f3a81e843f2a0fe08467cb88ebd419eb9b998f9
David Vo <datcv@hotmail.com>
2020-03-03 17:24:35 +1100

    Maximum Profit From Sale Of Wines

---
+++ RandomSource/MaximumProfitFromSaleOfWines/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaximumProfitFromSaleOfWines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new int[] { 2, 4, 6, 2, 5 };
+            Console.WriteLine("Input: [" + string.Join(", ", input) + "]");
+            Console.WriteLine("Max Profit: " + MaxProfit(input, 0, input.Length - 1));
+        }
+
+        private static IDictionary<string, int> profitDict = new Dictionary<string, int>();
+        public static int MaxProfit(int[] prices, int l, int r)
+        {
+            string key = $"{l}#{r}";
+            if (profitDict.TryGetValue(key, out var v))
+            {
+                return v;
+            }
+
+            if (l == r)
+            {
+                profitDict[key] = prices[l] * prices.Length;
+            }
+            else
+            {
+                profitDict[key] = Max(prices[l] * (l + prices.Length - r) + MaxProfit(prices, l + 1, r), prices[r] * (l + prices.Length - r) + MaxProfit(prices, l, r - 1));
+            }
+
+            return profitDict[key];
+        }
+
+        private static int Max(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+    }
+}
