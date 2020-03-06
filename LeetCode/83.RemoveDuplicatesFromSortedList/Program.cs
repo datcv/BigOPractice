7ca0699693f26af5fe76f2a2224c39d0e6f7a5ce
David Vo <datcv@hotmail.com>
2020-03-06 16:49:17 +1100

    Remove Duplicates from Sorted List

---
+++ LeetCode/83.RemoveDuplicatesFromSortedList/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace _83.RemoveDuplicatesFromSortedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lst = new ListNode(1)
+            {
+                next = new ListNode(1)
+                {
+                    next = new ListNode(2)
+                    {
+                        next = new ListNode(3)
+                        {
+                            next = new ListNode(3)
+                        }
+                    }
+                }
+            };
+
+            Console.WriteLine("Input: ");
+            lst.Print();
+
+            var l = DeleteDuplicates(lst);
+
+            Console.WriteLine();
+            Console.WriteLine("Output: ");
+            l.Print();
+        }
+
+        public static ListNode DeleteDuplicates(ListNode head)
+        {
+            var temp = head;
+
+            while (temp != null && temp.next != null)
+            {
+                if (temp.val == temp.next.val)
+                {
+                    temp.next = temp.next.next;
+                }
+                else
+                {
+                    temp = temp.next;
+                }
+
+            }
+
+            return head;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+
+        public void Print()
+        {
+            var t = this;
+            while(t != null)
+            {
+                Console.Write($" {t.val}");
+                t = t.next;
+            }
+        }
+    }
+}
---
+++ LeetCode/83.RemoveDuplicatesFromSortedList/README.md
@@ -0,0 +1,17 @@
+# Remove Duplicates from Sorted List
+Given a sorted linked list, delete all duplicates such that each element appear only once.  
+
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.  
+
+Note that you cannot sell a stock before you buy one.  
+**Example 1:**  
+```
+Input: 1->1->2  
+Output: 1->2  
+```
+
+**Example 2:**  
+```
+Input: 1->1->2->3->3  
+Output: 1->2->3  
+```
