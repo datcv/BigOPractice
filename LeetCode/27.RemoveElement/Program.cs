319e2c5cb330661950ec548e3895c406915e3538
David Vo <datcv@hotmail.com>
2020-03-27 22:40:34 +1100

    Remove Element

---
+++ LeetCode/27.RemoveElement/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _27.RemoveElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = RemoveElement(new int[]{1, 2, 2, 3, 4, 3}, 3);
+            Console.WriteLine(t);
+        }
+
+        public static int RemoveElement(int[] nums, int val) 
+        {
+            int l = 0, r = nums.Length - 1;
+            while(l <= r)
+            {
+                if (nums[l] != val && nums[r] == val)
+                {
+                    l++;
+                    r--;
+                }
+                else if (nums[l] != val)
+                {
+                    l++;
+                }
+                else
+                {
+                    nums[l] = nums[r];
+                    r--;
+                }
+            }
+        
+            return l;
+        }
+    }
+}
