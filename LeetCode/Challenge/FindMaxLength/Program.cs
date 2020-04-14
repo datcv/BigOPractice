6f3b84bca706f5917ed41cc4cb20aec634984036
David Vo <datcv@hotmail.com>
2020-04-15 09:34:13 +1000

    FindMaxLength

---
+++ LeetCode/Challenge/FindMaxLength/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindMaxLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(FindMaxLength(new int[]{0,0,1,0,0,0,1,1}));
+        }
+
+        public static int FindMaxLength(int[] nums) {
+            int count = 0, max = 0;
+            var dict = new Dictionary<int, int>(){{0, -1}};
+            for(var i = 0; i < nums.Length; i++)
+            {
+                count += (nums[i] == 0 ? -1 : 1);
+                if (dict.ContainsKey(count)){
+                    max = Math.Max(max, i - dict[count]);
+                }
+                else{
+                    dict[count] = i;
+                }
+            }
+            
+            return max;
+        }
+    }
+}
